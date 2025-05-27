b83fa4399b4492a05aa70fd0d9a263049a254036
Mason-Zaccaro <65304987+Mason-Zaccaro@users.noreply.github.com>
2025-05-27 16:42:05 +0300

    ASP-ADO.NET КТ-1

---
+++ ASP-ADO.NET/MvcApp1/Controllers/PageController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcApp1.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Welcome()
+        {
+            ViewBag.CurrentDate = DateTime.Now;
+            return View();
+        }
+
+        public IActionResult Greet(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "Гость";
+            }
+
+            ViewBag.UserName = name;
+            ViewBag.CurrentTime = DateTime.Now.ToString("HH:mm:ss");
+            ViewBag.CurrentDate = DateTime.Now.ToString("dd.MM.yyyy");
+
+            return View();
+        }
+    }
+}
